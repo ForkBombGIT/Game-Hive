b0367237e86d3451297d4682e9adf38d3d6cb1e9
Epoco <cream_egg@live.com>
2018-02-11 22:43:40 -0500

    Added Dropdown to topbar

--- app/src/main/java/forkbomb/gamehive/Game_Hive.java
+++ app/src/main/java/forkbomb/gamehive/Game_Hive.java
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -13,6 +15,7 @@ public class Game_Hive extends AppCompatActivity {
     //var to hold the top toolbar
     Toolbar toptoolbar;
 
+    //on create function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,14 @@ public class Game_Hive extends AppCompatActivity {
         //sets up toolbar
         toptoolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toptoolbar);
+    }
+
+    //drop down menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.navigation, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
